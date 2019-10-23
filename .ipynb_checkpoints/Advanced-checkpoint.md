c0f382f4351a7c0d3f237d42f699e72fbef68424
gafarina <gaston.farina@gmail.com>
2019-10-23 09:26:12 -0300

    nuevos pasos

--- .ipynb_checkpoints/Advanced-checkpoint.md
+++ .ipynb_checkpoints/Advanced-checkpoint.md
@@ -1,5 +1,18 @@
-# Compute the mean and volatility return
-# Compute the skweness and kuttosis
-# Compute the annualized return and volatility
-# Drawdown for a serie 
-### I want to create this computations to a pandas dataframe
+## Siguientes Passos
+
+* revisar la documentacion de las funciones
+
+* Crear el sharpe ratio (despues)
+
+* Get the date for drawdown(lab103)
+
+* Ver el tema de los logs returns (Para despues)
+
+* VAR,CVAR,Cornish Var(lab 106, clase 106)
+
+
+## Detalles
+
+* Crear en la clase de retornos Var,CVAR y otros Var
+
+* 
--- .ipynb_checkpoints/Class-checkpoint.py
+++ .ipynb_checkpoints/Class-checkpoint.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.stats
 
 class GetAndFormatTheData:
     #def __init__(self,Ticker):
@@ -53,6 +54,69 @@ class ReturnOps():
         '''
         ret = (1+PerReturn)**PerInAYear - 1
         return ret
+    
+    def MeanReturn(self,Base):
+        Base = Base.mean()
+        return Base
+    
+    def VolReturn(self,Base):
+        Base = Base.std()
+        return Base
+    
+    def Sesgo(self,Base):
+        DesMean = Base-Base.mean()
+        sigma_Base = Base.std(ddof=0)
+        exp = (DesMean**3).mean()
+        return exp/sigma_Base**3
+    
+    def kurtosis(self,Base):
+        """
+        Alternative to scipy.stats.kurtosis()
+        Computes the kurtosis of the supplied Series or DataFrame
+        Returns a float or a Series
+        """
+        demeaned_Base = Base - Base.mean()
+        # use the population standard deviation, so set dof=0
+        sigma_Base = Base.std(ddof=0)
+        exp = (demeaned_Base**4).mean()
+        return exp/sigma_Base**4
+    
+    def drawdown(self,return_series: pd.Series):
+        """Takes a time series of asset returns.
+           returns a DataFrame with columns for
+           the wealth index, 
+           the previous peaks, and 
+           the percentage drawdown
+        """
+        wealth_index = 1000*(1+return_series).cumprod()
+        previous_peaks = wealth_index.cummax()
+        drawdowns = (wealth_index - previous_peaks)/previous_peaks
+        return pd.DataFrame({"Wealth": wealth_index, 
+                             "Previous Peak": previous_peaks, 
+                             "Drawdown": drawdowns})
+    
+    def AnnualizedVol(self,Base,Per):
+        Base = self.VolReturn(Base)*np.sqrt(Per)
+        return Base
+    
+    def AnnualizedMean(self,Base,DaysBase):
+        Days = Base.shape[0]
+        Base = (Base+1).prod()**(DaysBase/Days) - 1
+        return Base
+    
+    def is_normal(self,r,level=0.01):
+        """
+        Applies the Jarque-Bera test to determine if a Series is normal or not
+        Test is applied at the 1% level by default
+        Returns True if the hypothesis of normality is accepted, False otherwise
+        """
+        if isinstance(r, pd.DataFrame):
+            return r.aggregate(self.is_normal)
+        else:
+            statistic, p_value = scipy.stats.jarque_bera(r)
+            return p_value > level
+        
+
         
      
         
